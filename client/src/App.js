9ae55c88323326ad7daf620f9e801508677cf305
tallan groberg <tallan.taven@gmail.com>
2019-10-14 15:24:37 -0600

    initialize

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const App = props => {
+  return (
+    <div>
+      blank
+    </div>
+  );
+};
+
+export default App;
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
+app.use(express.json())
+app.use(morgan('dev'))
+
+mongoose.connect('mongodb://localhost:27017/blogitupdb', {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+}, console.log('db connected...'))
+
+app.use( (err,req,res,next) => {
+  console.log(err)
+  res.send({errMsg: err.error.message})
+})
+
+app.listen(4444, () => {
+  console.log(`live! on 4444`)
+})
